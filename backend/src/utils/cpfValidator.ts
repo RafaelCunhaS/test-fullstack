03c7b340bf3c67e3cbadaf3be9995aa1a873ab43
Rafael Santos <rafaelcs_94@hotmail.com>
2024-03-16 22:21:01 -0300

    backend: feat: create schema validation

---
+++ backend/src/utils/cpfValidator.ts
@@ -0,0 +1,19 @@
+function isValidCPF(value: string): boolean {
+  if (typeof value !== 'string') {
+    return false
+  }
+
+  value = value.replace(/[^\d]+/g, '')
+
+  if (value.length !== 11 || !(value.match(/(\d)\1{10}/) == null)) {
+    return false
+  }
+
+  const values = value.split('').map((el) => +el)
+  const rest = (count: number): number =>
+    ((values.slice(0, count - 12).reduce((soma, el, index) => soma + el * (count - index), 0) * 10) % 11) % 10
+
+  return rest(10) === values[9] && rest(11) === values[10]
+}
+
+export default isValidCPF
