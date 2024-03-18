5d0b5e5f014b7c6e7999bb3a8bb8b797d0694ab5
Rafael Santos <rafaelcs_94@hotmail.com>
2024-03-18 16:08:59 -0300

    frontend: feat: create loading component

---
+++ frontend/src/components/Loading/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss'
+
+export function LoadingSpinner() {
+  return (
+    <div className={styles.spinnerContainer}>
+      <div className={styles.loadingSpinner}></div>
+    </div>
+  )
+}
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -7,19 +7,23 @@ import ClientCard from '../../components/ClientCard'
 import { useNavigate } from 'react-router-dom'
 import ClientHeader from '../../components/ClientHeader'
 import { Pagination } from '../../components/Pagination'
+import { LoadingSpinner } from '../../components/Loading'
 
 export default function Home() {
   const [clients, setClients] = useState<IDataForm[]>([])
   const [currentPage, setCurrentPage] = useState(1)
   const [totalPages, setTotalPages] = useState(1)
+  const [isLoading, setIsLoading] = useState(false)
   const navigate = useNavigate()
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true)
       const data = await listClients(currentPage - 1)
       if (data) {
         setTotalPages(data.totalPages)
         setClients(data.data)
+        setIsLoading(false)
       }
     }
     fetchData()
@@ -35,13 +39,23 @@ export default function Home() {
         </div>
         <Button title="Novo cliente" onClick={() => navigate('/create')} />
       </div>
-      <ul className={styles.list}>
-        {clients.map((client) => (
-          <ClientCard key={client.id} client={client} />
-        ))}
-      </ul>
-      <p className={styles.text}>Exibindo {clients.length} clientes</p>
-      <Pagination setCurrentPage={(page) => setCurrentPage(page)} totalPages={totalPages} currentPage={currentPage} />
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : (
+        <div>
+          <ul className={styles.list}>
+            {clients.map((client) => (
+              <ClientCard key={client.id} client={client} />
+            ))}
+          </ul>
+          <p className={styles.text}>Exibindo {clients.length} clientes</p>
+          <Pagination
+            setCurrentPage={(page) => setCurrentPage(page)}
+            totalPages={totalPages}
+            currentPage={currentPage}
+          />
+        </div>
+      )}
     </main>
   )
 }
